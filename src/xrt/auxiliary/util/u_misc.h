e57e8583513b725c97a39d2bff31eeb6c7c23c3c
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-03-21 15:18:44 -0500

    aux/util: Add U_TYPED_CALLOC macro to wrap calloc and casting.

--- src/xrt/auxiliary/util/u_misc.h
+++ src/xrt/auxiliary/util/u_misc.h
@@ -12,6 +12,18 @@
 extern "C" {
 #endif
 
+/*!
+ * Allocate and zero the space required for some type, and cast the return type
+ * appropriately.
+ */
+#define U_TYPED_CALLOC(TYPE) ((TYPE *)calloc(1, sizeof(TYPE)))
+
+/*!
+ * Allocate and zero the space required for some type, and cast the return type
+ * appropriately.
+ */
+#define U_TYPED_ARRAY_CALLOC(TYPE, COUNT)                                      \
+	((TYPE *)calloc((COUNT), sizeof(TYPE)))
 
 
 #ifdef __cplusplus
