43dfb400a647d312b122da989813a1ed635c61d4
Jakob Bornecrantz <jakob@collabora.com>
2019-04-30 14:41:17 +0100

    prober: Add small helper program to debug st/prober

---
+++ src/xrt/targets/prober/main.c
@@ -0,0 +1,105 @@
+// Copyright 2019, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  A program to help test the probing code in Monado.
+ * @author Jakob Bornecrantz <jakob@collabora.com>
+ */
+
+#include <string.h>
+#include <stdio.h>
+
+#include "target_lists.h"
+
+
+static int
+ps3_eye_found(struct xrt_prober *xp,
+              struct xrt_prober_device **devices,
+              size_t index,
+              struct xrt_device **out_xdev)
+{
+	printf("Found PS3 Eye!\n");
+	return 0;
+}
+
+struct xrt_prober_entry quirks_list[] = {
+    {0x1415, 0x2000, ps3_eye_found, "PS3 Eye"},
+    {0x0000, 0x0000, NULL, NULL}, // Terminate
+};
+
+struct xrt_prober_entry *entry_lists[] = {
+    quirks_list, target_entry_list,
+    NULL, // Terminate
+};
+
+struct xrt_prober_entry_lists list = {
+    entry_lists,
+    target_auto_list,
+    NULL,
+};
+
+int
+xrt_prober_create(struct xrt_prober **out_xp)
+{
+	return xrt_prober_create_with_lists(out_xp, &list);
+}
+
+int
+do_exit(struct xrt_prober **xp, int ret)
+{
+	if (*xp != NULL) {
+		(*xp)->destroy(xp);
+	}
+
+	printf(" :: Exiting '%i'\n", ret);
+	return ret;
+}
+
+int
+main(int argc, const char **argv)
+{
+	struct xrt_device *xdev = NULL;
+	struct xrt_prober *p = NULL;
+	int ret = 0;
+
+	printf(" :: Creating prober!\n");
+
+	ret = xrt_prober_create(&p);
+	if (ret != 0) {
+		return ret;
+	}
+
+	printf(" :: Probing!\n");
+
+	ret = p->probe(p);
+	if (ret != 0) {
+		return do_exit(&p, ret);
+	}
+
+	printf(" :: Dumping!\n");
+
+	ret = p->dump(p);
+	if (ret != 0) {
+		do_exit(&p, ret);
+	}
+
+	printf(" :: Selecting device!\n");
+
+	ret = p->select(p, &xdev);
+	if (ret != 0) {
+		do_exit(&p, ret);
+	}
+	if (xdev == NULL) {
+		printf("\tNo device found! :(\n");
+		return do_exit(&p, -1);
+	}
+
+	printf("\tFound '%s'\n", xdev->name);
+
+	xdev->destroy(xdev);
+	xdev = NULL;
+
+	printf(" :: All ok, shutting down.\n");
+
+	return do_exit(&p, 0);
+}
