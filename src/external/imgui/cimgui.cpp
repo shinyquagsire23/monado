8301bd8d4cd98df3bbefcdafc411f2e03081a965
Jakob Bornecrantz <jakob@collabora.com>
2019-10-22 21:37:03 +0100

    external: Refactor out sdl functions from main cimguo.cpp

--- src/external/imgui/cimgui.cpp
+++ src/external/imgui/cimgui.cpp
@@ -4515,34 +4515,6 @@ CIMGUI_API void ImVector_ImWchar_UnInit(ImVector_ImWchar* p)
 
 
 /////////////////////////extra Monado functions
-#include "imgui_impl_sdl.h"
-CIMGUI_API bool igImGui_ImplSDL2_InitForOpenGL(SDL_Window* window, void* sdl_gl_context)
-{
-	return ImGui_ImplSDL2_InitForOpenGL(window, sdl_gl_context);
-}
-#if 0
-CIMGUI_API bool igImGui_ImplSDL2_InitForVulkan(SDL_Window* window)
-{
-	return ImGui_ImplSDL2_InitForVulkan(window);
-}
-CIMGUI_API bool igImGui_ImplSDL2_InitForD3D(SDL_Window* window)
-{
-	return ImGui_ImplSDL2_InitForD3D(window);
-}
-#endif
-CIMGUI_API void igImGui_ImplSDL2_Shutdown()
-{
-	ImGui_ImplSDL2_Shutdown();
-}
-CIMGUI_API void igImGui_ImplSDL2_NewFrame(SDL_Window* window)
-{
-	ImGui_ImplSDL2_NewFrame(window);
-}
-CIMGUI_API bool igImGui_ImplSDL2_ProcessEvent(const SDL_Event* event)
-{
-	return ImGui_ImplSDL2_ProcessEvent(event);
-}
-
 #include "imgui_impl_opengl3.h"
 CIMGUI_API bool igImGui_ImplOpenGL3_Init(const char* glsl_version)
 {
---
+++ src/external/imgui/cimgui_sdl.cpp
@@ -0,0 +1,34 @@
+#include "./imgui/imgui.h"
+#include "cimgui.h"
+
+#include "./imgui/imgui_internal.h"
+
+#include "imgui_impl_sdl.h"
+
+
+CIMGUI_API bool igImGui_ImplSDL2_InitForOpenGL(SDL_Window* window, void* sdl_gl_context)
+{
+	return ImGui_ImplSDL2_InitForOpenGL(window, sdl_gl_context);
+}
+#if 0
+CIMGUI_API bool igImGui_ImplSDL2_InitForVulkan(SDL_Window* window)
+{
+	return ImGui_ImplSDL2_InitForVulkan(window);
+}
+CIMGUI_API bool igImGui_ImplSDL2_InitForD3D(SDL_Window* window)
+{
+	return ImGui_ImplSDL2_InitForD3D(window);
+}
+#endif
+CIMGUI_API void igImGui_ImplSDL2_Shutdown()
+{
+	ImGui_ImplSDL2_Shutdown();
+}
+CIMGUI_API void igImGui_ImplSDL2_NewFrame(SDL_Window* window)
+{
+	ImGui_ImplSDL2_NewFrame(window);
+}
+CIMGUI_API bool igImGui_ImplSDL2_ProcessEvent(const SDL_Event* event)
+{
+	return ImGui_ImplSDL2_ProcessEvent(event);
+}
