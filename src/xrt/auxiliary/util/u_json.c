eda46d3bead0099ab5ae6646f4cd393885d01532
Jakob Bornecrantz <jakob@collabora.com>
2019-11-08 11:25:36 +0000

    util: Add some wrapper files around cJSON
    This makes cJSON files be accessible from everywhere in Monado,
    and adds a nice place to add common helpers.

---
+++ src/xrt/auxiliary/util/u_json.c
@@ -0,0 +1,10 @@
+// Copyright 2019, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  Tiny JSON wrapper around cJSON.
+ * @author Jakob Bornecrantz <jakob@collabora.com>
+ * @ingroup aux_util
+ */
+
+#include "cjson/cJSON.c"
---
+++ src/xrt/auxiliary/util/u_json.h
@@ -0,0 +1,14 @@
+// Copyright 2019, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  Tiny JSON wrapper around cJSON header.
+ * @author Jakob Bornecrantz <jakob@collabora.com>
+ * @ingroup aux_util
+ */
+
+#pragma once
+
+#include "xrt/xrt_compiler.h"
+
+#include "cjson/cJSON.h"
