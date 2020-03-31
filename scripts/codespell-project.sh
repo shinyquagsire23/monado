e7bec75d705ded9a8abd7b6481417f6f70ce8a27
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-03-31 11:25:45 -0500

    scripts: Add more exceptions to codespell.

--- scripts/codespell-project.sh
+++ scripts/codespell-project.sh
@@ -19,7 +19,7 @@
 set -e
 
 # Comma-delimited list of words for codespell to not try to correct.
-IGNORE_WORDS_LIST="ang,sinc"
+IGNORE_WORDS_LIST="ang,sinc,sie,inout"
 
 SCRIPTDIR=$(cd $(dirname $0) && pwd)
 
