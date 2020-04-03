6b05c216cea68261d44c268a8ea6834b6d8a4509
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2020-04-03 17:25:19 +0200

    c/settings: Add a bunch of display strings to NVIDIA whitelist.
    Whitelist Oculus DK1, DK2 and CV1.
    Whitelist Vive Pro and Valve Index.

--- src/xrt/compositor/main/comp_settings.h
+++ src/xrt/compositor/main/comp_settings.h
@@ -27,6 +27,9 @@ extern "C" {
 XRT_MAYBE_UNUSED static const char *NV_DIRECT_WHITELIST[] = {
     "Sony SIE  HMD *08",
     "HTC Corporation HTC-VIVE",
+    "HTC Corporation VIVE Pro",
+    "Oculus VR Inc. Rift", /* Matches DK1, DK2 and CV1 */
+    "Valve Corporation Index HMD",
 };
 
 /*!
