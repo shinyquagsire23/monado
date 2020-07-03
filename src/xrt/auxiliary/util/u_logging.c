38685eba381f52f7d0bd257cc6a91ed05d445f9b
Jakob Bornecrantz <jakob@collabora.com>
2020-07-03 14:27:39 +0100

    u/log: Add logging helpers

---
+++ src/xrt/auxiliary/util/u_logging.c
@@ -0,0 +1,83 @@
+// Copyright 2019-2020, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  Logging functions.
+ * @author Jakob Bornecrantz <jakob@collabora.com>
+ * @ingroup aux_util
+ */
+
+#include "util/u_logging.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+
+/*
+ *
+ * Helper functions.
+ *
+ */
+
+static void
+print_prefix(const char *func, enum u_logging_level level)
+{
+	if (level != U_LOGGING_RAW && func != NULL) {
+		fprintf(stderr, "%s ", func);
+	}
+
+	switch (level) {
+	case U_LOGGING_TRACE: fprintf(stderr, "TRACE: "); break;
+	case U_LOGGING_DEBUG: fprintf(stderr, "DEBUG: "); break;
+	case U_LOGGING_INFO: fprintf(stderr, "INFO: "); break;
+	case U_LOGGING_WARN: fprintf(stderr, "WARN: "); break;
+	case U_LOGGING_ERROR: fprintf(stderr, "ERROR: "); break;
+	case U_LOGGING_RAW: break;
+	default: break;
+	}
+}
+
+
+/*
+ *
+ * 'Exported' functions.
+ *
+ */
+
+void
+u_log(const char *file,
+      int line,
+      const char *func,
+      enum u_logging_level level,
+      const char *format,
+      ...)
+{
+	print_prefix(func, level);
+
+	va_list args;
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+
+	fprintf(stderr, "\n");
+}
+
+void
+u_log_xdev(const char *file,
+           int line,
+           const char *func,
+           enum u_logging_level level,
+           struct xrt_device *xdev,
+           const char *format,
+           ...)
+{
+	print_prefix(func, level);
+
+	va_list args;
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+
+	fprintf(stderr, "\n");
+}
---
+++ src/xrt/auxiliary/util/u_logging.h
@@ -0,0 +1,116 @@
+// Copyright 2020, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  Tiny JSON wrapper around cJSON header.
+ * @author Jakob Bornecrantz <jakob@collabora.com>
+ * @ingroup aux_util aux_log
+ */
+
+
+#pragma once
+
+#include "xrt/xrt_compiler.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+struct xrt_device;
+
+
+/*!
+ * @defgroup aux_log Logging functions.
+ */
+
+/*!
+ * @ingroup aux_util aux_log
+ * @{
+ */
+
+/*!
+ * For places where you really just want printf, prints a new-line.
+ */
+#define U_LOG_RAW(...)                                                         \
+	do {                                                                   \
+		u_log(__FILE__, __LINE__, __func__, U_LOGGING_RAW,             \
+		      __VA_ARGS__);                                            \
+	} while (false)
+
+#define U_LOG(level, ...)                                                      \
+	do {                                                                   \
+		u_log(__FILE__, __LINE__, __func__, level, __VA_ARGS__);       \
+	} while (false)
+
+#define U_LOG_XDEV(level, xdev, ...)                                           \
+	do {                                                                   \
+		u_log_xdev(__FILE__, __LINE__, __func__, level, xdev,          \
+		           __VA_ARGS__);                                       \
+	} while (false)
+
+#define U_LOG_XDEV_IFL(level, cond_level, xdev, ...)                           \
+	do {                                                                   \
+		if (cond_level <= level) {                                     \
+			u_log_xdev(__FILE__, __LINE__, __func__, level, xdev,  \
+			           __VA_ARGS__);                               \
+		}                                                              \
+	} while (false)
+
+// clang-format off
+#define U_LOG_T(...) U_LOG(U_LOGGING_TRACE, __VA_ARGS__)
+#define U_LOG_D(...) U_LOG(U_LOGGING_DEBUG, __VA_ARGS__)
+#define U_LOG_I(...) U_LOG(U_LOGGING_INFO, __VA_ARGS__)
+#define U_LOG_W(...) U_LOG(U_LOGGING_WARN, __VA_ARGS__)
+#define U_LOG_E(...) U_LOG(U_LOGGING_ERROR, __VA_ARGS__)
+
+#define U_LOG_XDEV_IFL_T(xdev, cond_level, ...) U_LOG_XDEV_IFL(U_LOGGING_TRACE, cond_level, xdev, __VA_ARGS__)
+#define U_LOG_XDEV_IFL_D(xdev, cond_level, ...) U_LOG_XDEV_IFL(U_LOGGING_DEBUG, cond_level, xdev, __VA_ARGS__)
+#define U_LOG_XDEV_IFL_I(xdev, cond_level, ...) U_LOG_XDEV_IFL(U_LOGGING_INFO, cond_level, xdev, __VA_ARGS__)
+#define U_LOG_XDEV_IFL_W(xdev, cond_level, ...) U_LOG_XDEV_IFL(U_LOGGING_WARN, cond_level, xdev, __VA_ARGS__)
+#define U_LOG_XDEV_IFL_E(xdev, cond_level, ...) U_LOG_XDEV_IFL(U_LOGGING_ERROR, cond_level, xdev, __VA_ARGS__)
+
+#define U_LOG_XDEV_T(xdev, ...) U_LOG_XDEV(U_LOGGING_TRACE, xdev, __VA_ARGS__)
+#define U_LOG_XDEV_D(xdev, ...) U_LOG_XDEV(U_LOGGING_DEBUG, xdev, __VA_ARGS__)
+#define U_LOG_XDEV_I(xdev, ...) U_LOG_XDEV(U_LOGGING_INFO, xdev, __VA_ARGS__)
+#define U_LOG_XDEV_W(xdev, ...) U_LOG_XDEV(U_LOGGING_WARN, xdev, __VA_ARGS__)
+#define U_LOG_XDEV_E(xdev, ...) U_LOG_XDEV(U_LOGGING_ERROR, xdev, __VA_ARGS__)
+// clang-format on
+
+enum u_logging_level
+{
+	U_LOGGING_TRACE,
+	U_LOGGING_DEBUG,
+	U_LOGGING_INFO,
+	U_LOGGING_WARN,
+	U_LOGGING_ERROR,
+	U_LOGGING_RAW, //!< Special level for raw printing, prints a new-line.
+};
+
+void
+u_log(const char *file,
+      int line,
+      const char *func,
+      enum u_logging_level level,
+      const char *format,
+      ...) XRT_PRINTF_FORMAT(5, 6);
+
+void
+u_log_xdev(const char *file,
+           int line,
+           const char *func,
+           enum u_logging_level level,
+           struct xrt_device *xdev,
+           const char *format,
+           ...) XRT_PRINTF_FORMAT(6, 7);
+
+
+/*!
+ * @}
+ */
+
+
+#ifdef __cplusplus
+}
+#endif
