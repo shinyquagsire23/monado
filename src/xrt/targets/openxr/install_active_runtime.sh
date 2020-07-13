605e4e2027396673da97ad5e2760d1f080bd43b3
Christoph Haag <christoph.haag@collabora.com>
2020-07-13 14:08:34 +0200

    build: Fix installing active_runtime.json on meson without DESTDIR
    The -u setting for bash reported an error if DESTDIR is not set.
    
    The syntax ${A-foo} returns the content of A if set, or the default "foo" if not,
    "foo" being the empty string in this case.

--- src/xrt/targets/openxr/install_active_runtime.sh
+++ src/xrt/targets/openxr/install_active_runtime.sh
@@ -4,11 +4,11 @@
 #
 # Used by the Meson build only.
 
-sysconfdir="$DESTDIR"/"$1"
+sysconfdir="${DESTDIR-}"/"$1"
 manifest="$2"
 xrversion="$3"
 
 runtime_path="$sysconfdir"/xdg/openxr/"$xrversion"/active_runtime.json
 
 mkdir -p "$sysconfdir"/xdg/openxr/"$xrversion"
-ln -s "$manifest" "$runtime_path"
+ln -sf "$manifest" "$runtime_path"
