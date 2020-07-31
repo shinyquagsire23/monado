cbb1dd82da79eb408e965ff70c64f936139cd298
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2020-07-31 12:12:31 +0200

    d/vive: Remove unnesseccary clang-format off.

--- src/xrt/drivers/vive/vive.h
+++ src/xrt/drivers/vive/vive.h
@@ -16,10 +16,8 @@
  *
  */
 
-// clang-format off
 #define VIVE_TRACE(d, ...) U_LOG_XDEV_IFL_T(&d->base, d->ll, __VA_ARGS__)
 #define VIVE_DEBUG(d, ...) U_LOG_XDEV_IFL_D(&d->base, d->ll, __VA_ARGS__)
 #define VIVE_INFO(d, ...) U_LOG_XDEV_IFL_I(&d->base, d->ll, __VA_ARGS__)
 #define VIVE_WARN(d, ...) U_LOG_XDEV_IFL_W(&d->base, d->ll, __VA_ARGS__)
 #define VIVE_ERROR(d, ...) U_LOG_XDEV_IFL_E(&d->base, d->ll, __VA_ARGS__)
-// clang-format on
