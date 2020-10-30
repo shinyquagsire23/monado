240b8f3047d3da3a22b8dfc95de3481600052347
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-10-30 16:13:05 -0500

    t/oxr_android: Include library attribution/notices in "about" activity.

--- src/xrt/targets/openxr_android/src/main/java/org/freedesktop/monado/openxr_runtime/MainActivity.java
+++ src/xrt/targets/openxr_android/src/main/java/org/freedesktop/monado/openxr_runtime/MainActivity.java
@@ -13,8 +13,11 @@ import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.mikepenz.aboutlibraries.LibsBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +35,20 @@ public class MainActivity extends AppCompatActivity {
         int status = VrModeStatus.detectStatus(this, BuildConfig.APPLICATION_ID);
         VrModeStatus statusFrag = VrModeStatus.newInstance(status);
         fragmentTransaction.add(R.id.statusFrame, statusFrag, null);
+
+
+        Fragment libsFragment = (new LibsBuilder())
+                .withFields(R.string.class.getFields())
+
+                // We do this ourselves bigger
+                .withAboutIconShown(false)
+                // Let the fragment show our version
+                .withAboutVersionShown(true)
+
+                // Not sure why you'd do this without license info
+                .withLicenseShown(true)
+                .supportFragment();
+        fragmentTransaction.add(R.id.aboutFrame, libsFragment, null);
         fragmentTransaction.commit();
     }
 }
