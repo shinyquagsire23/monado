3328688f730d8342db85c7fd2a975c5fdd03acc4
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-11-09 15:24:44 -0600

    t/service-lib: Simplify.

--- src/xrt/targets/service-lib/lib.cpp
+++ src/xrt/targets/service-lib/lib.cpp
@@ -12,25 +12,23 @@
 #include "jnipp.h"
 #include "jni.h"
 
-#include "wrap/android.os.h"
 #include "wrap/android.view.h"
 
 #include <android/native_window.h>
 #include <android/native_window_jni.h>
 
-using wrap::android::os::ParcelFileDescriptor;
 using wrap::android::view::Surface;
 
 extern "C" void
-Java_org_freedesktop_monado_ipc_MonadoImpl_nativeAddClient(
-    JNIEnv *env, jobject thiz, jobject parcel_file_descriptor)
+Java_org_freedesktop_monado_ipc_MonadoImpl_nativeAddClient(JNIEnv *env,
+                                                           jobject thiz,
+                                                           int fd)
 {
 	jni::init(env);
 	//! @todo do something!
 	// This may be the "entry point" of the native code, or we could already
 	// have another client running, etc.
 
-	ParcelFileDescriptor pfd(parcel_file_descriptor);
 	jni::Object monadoImpl(thiz);
 }
 
