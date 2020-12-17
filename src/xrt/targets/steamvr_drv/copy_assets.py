db2b77c9cb92d1c1b0ea0538e7142fe663201fcc
Christoph Haag <christoph.haag@collabora.com>
2020-12-17 16:14:07 +0100

    build/meson: Copy steamvr assets at build time, not configure time

--- src/xrt/targets/steamvr_drv/copy_assets.py
+++ src/xrt/targets/steamvr_drv/copy_assets.py
@@ -3,7 +3,10 @@
 # Copyright 2020, Collabora, Ltd.
 # SPDX-License-Identifier: BSL-1.0
 
-import os, sys, shutil
+import os
+import sys
+from distutils.dir_util import copy_tree
+from distutils.file_util import copy_file
 
 print(sys.argv[1], sys.argv[2], sys.argv[3])
 
@@ -11,21 +14,19 @@ is_file = sys.argv[1] == "FILE"
 is_dir = sys.argv[1] == "DIRECTORY"
 
 # get absolute input and output paths
-input_path = os.path.join(
-    os.getenv('MESON_SOURCE_ROOT'),
-    os.getenv('MESON_SUBDIR'),
-    sys.argv[2])
+input_path = sys.argv[2]
 
-output_path = os.path.join(
-    os.getenv('MESON_BUILD_ROOT'),
-    sys.argv[3])
+output_path = sys.argv[3]
 
 # make sure destination directory exists
 os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
 if is_file:
-    shutil.copyfile(input_path, output_path)
+    copy_file(input_path, output_path)
 elif is_dir:
-    shutil.copytree(input_path, output_path)
+    copy_tree(input_path, output_path)
+else:
+    print(sys.argv[1], "must be FILE or DIRECTORY")
+    sys.exit(1)
 
 print("Copying asset " + str(input_path) + " to " + str(output_path))
