ad1e809dfc5e621646de22afdbe5d8e6ea2981bf
Christoph Haag <christoph.haag@collabora.com>
2021-01-13 23:29:42 +0100

    comp: Extend nvidia VK_ERROR_INITIALIZATION_FAILED message
    The real cause for this failure seems to be an extended desktop to the direct mode
    display, not the allowhmd setting.

--- src/xrt/compositor/main/comp_window_direct.c
+++ src/xrt/compositor/main/comp_window_direct.c
@@ -271,7 +271,8 @@ comp_window_direct_acquire_xlib_display(struct comp_target_swapchain *cts,
 			           "This can be caused by the AllowHMD "
 			           "xorg.conf option. Please make sure that "
 			           "AllowHMD is not set (like in '99-HMD.conf' "
-			           "from OpenHMD).");
+			           "from OpenHMD) and that the desktop is not "
+			           "currently extended to this display.");
 		}
 	}
 	return ret;
