3df2708a5d826efeec43d7aaf1896b20bcb5334f
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-04-21 11:12:37 -0500

    st/gui: Add missing copyright header to file

--- src/xrt/state_trackers/gui/gui_stb.c
+++ src/xrt/state_trackers/gui/gui_stb.c
@@ -1,3 +1,10 @@
+// Copyright 2020, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  Compile the STB image write implementation.
+ * @author Jakob Bornecrantz <jakob@collabora.com>
+ */
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
