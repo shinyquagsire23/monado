e7d047f956744d46f9311d087babcad5523c2559
Christoph Haag <christoph.haag@collabora.com>
2021-08-31 02:09:11 +0200

    aux/vk: debug print preferred/required dedicated allocation

--- src/xrt/auxiliary/vk/vk_image_allocator.c
+++ src/xrt/auxiliary/vk/vk_image_allocator.c
@@ -219,7 +219,9 @@ create_image(struct vk_bundle *vk, const struct xrt_swapchain_create_info *info,
 	vk->vkGetImageMemoryRequirements2(vk->device, &memory_requirements_info, &memory_requirements);
 
 	VkBool32 use_dedicated_allocation = memory_dedicated_requirements.requiresDedicatedAllocation != VK_FALSE;
-	U_LOG_D("create_image: Use dedicated allocation: %d", use_dedicated_allocation);
+	U_LOG_D("create_image: Use dedicated allocation: %d (preferred: %d, required: %d)", use_dedicated_allocation,
+	        memory_dedicated_requirements.prefersDedicatedAllocation,
+	        memory_dedicated_requirements.requiresDedicatedAllocation);
 
 	/*
 	 * Create and bind the memory.
