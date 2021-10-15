bdce9087ae9bdad31c0f3168240b271fafa6331e
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-10-15 14:55:55 -0500

    ci: Update to build packages for Focal, Hirsute, Impish, Buster, and Bullseye

---
+++ .gitlab-ci/debian_buster_container_prep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright 2018-2020, Collabora, Ltd. and the Monado contributors
+# SPDX-License-Identifier: BSL-1.0
+
+(
+    cd $(dirname $0)
+    bash ./install-cross.sh
+)
+(
+    cd $(dirname $0)
+    bash ./build-openxr-openhmd.sh
+)
