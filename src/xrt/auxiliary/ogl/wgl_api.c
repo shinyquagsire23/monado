fc0f390a874c77fff80a7f302521c1d09bbe3896
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-10-15 16:16:59 -0500

    aux/ogl: Add WGL wrapper to the build.

---
+++ src/xrt/auxiliary/ogl/wgl_api.c
@@ -0,0 +1,10 @@
+// Copyright 2021-2022, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  WGL API wrapper.
+ * @author Ryan Pavlik <ryan.pavlik@collabora.com>
+ * @ingroup aux_ogl
+ */
+
+#include "../../external/glad/src/wgl.c"
---
+++ src/xrt/auxiliary/ogl/wgl_api.h
@@ -0,0 +1,14 @@
+// Copyright 2021-2022, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  WGL API wrapper header.
+ * @author Ryan Pavlik <ryan.pavlik@collabora.com>
+ * @ingroup aux_ogl
+ */
+
+#pragma once
+
+#include <xrt/xrt_windows.h>
+
+#include "glad/wgl.h"
