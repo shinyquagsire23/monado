c7549c86809c26cc453370105d6824a49c12ee25
Jan Schmidt <jan@centricular.com>
2021-12-03 16:03:32 +1100

    d/wmr: Apply HMD IMU correction from calibration.
    Apply the mix_matrix and bias_offsets adjustments for
    IMU readings.

--- src/xrt/drivers/wmr/wmr_hmd.c
+++ src/xrt/drivers/wmr/wmr_hmd.c
@@ -264,11 +264,15 @@ hololens_sensors_read_packets(struct wmr_hmd *wh)
 		struct xrt_vec3 raw_accel[4];
 
 		for (int i = 0; i < 4; i++) {
-			struct xrt_vec3 sample;
-			vec3_from_hololens_gyro(wh->packet.gyro, i, &sample);
+			struct xrt_vec3 raw_sample, sample;
+			vec3_from_hololens_gyro(wh->packet.gyro, i, &raw_sample);
+			math_matrix_3x3_transform_vec3(&wh->config.sensors.gyro.mix_matrix, &raw_sample, &sample);
+			math_vec3_accum(&wh->config.sensors.gyro.bias_offsets, &sample);
 			math_quat_rotate_vec3(&wh->gyro_to_centerline.orientation, &sample, &raw_gyro[i]);
 
 			vec3_from_hololens_accel(wh->packet.accel, i, &sample);
+			math_matrix_3x3_transform_vec3(&wh->config.sensors.accel.mix_matrix, &raw_sample, &sample);
+			math_vec3_accum(&wh->config.sensors.accel.bias_offsets, &sample);
 			math_quat_rotate_vec3(&wh->accel_to_centerline.orientation, &sample, &raw_accel[i]);
 		}
 
