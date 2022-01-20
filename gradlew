4fb4d4d41c926e7d1c84a44f3144ac5bccddfc7d
Ryan Pavlik <ryan.pavlik@collabora.com>
2022-01-20 14:31:58 -0600

    t/oxr_android: add licenses for gradlew
    Only cherry-pick license part fart from origin commit.

--- gradlew
+++ gradlew
@@ -1,4 +1,5 @@
 #!/bin/sh
+# SPDX-License-Identifier: Apache-2.0
 
 #
 # Copyright © 2015-2021 the original authors.
--- gradlew.bat
+++ gradlew.bat
@@ -13,6 +13,7 @@
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
 @rem
+@rem SPDX-License-Identifier: Apache-2.0
 
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
