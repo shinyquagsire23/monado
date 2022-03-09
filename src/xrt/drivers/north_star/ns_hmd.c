a4167ee5008788a8c0215b8d02fa1fd81758fbef
Antonio Ospite <antonio.ospite@collabora.com>
2022-03-09 16:10:43 +0100

    d/ns: silence -Wunused-result warning, close config_file in error paths
    On some systems fread() is declared with  attribute warn_unused_result
    and this results in a build warning:
    
    -----------------------------------------------------------------------
    Building C object src/xrt/drivers/CMakeFiles/drv_ns.dir/north_star/ns_hmd.c.o
    .../src/xrt/drivers/north_star/ns_hmd.c: In function ‘ns_config_load’:
    .../src/xrt/drivers/north_star/ns_hmd.c:512:2: warning: ignoring return value of ‘fread’, declared with attribute warn_unused_result [-Wunused-result]
      512 |  fread(json, 1, file_size, config_file);
          |  ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    -----------------------------------------------------------------------
    
    Explicitly ignore the return value of that fread() to silence the
    warning.
    
    And while at it also close config_file in some error paths between
    fopen() and fclose() which where leaking the file pointer when jumping
    to the parse_error label.

--- src/xrt/drivers/north_star/ns_hmd.c
+++ src/xrt/drivers/north_star/ns_hmd.c
@@ -500,16 +500,18 @@ ns_config_load(struct ns_hmd *ns, const char *config_path)
 
 	if (file_size == 0) {
 		NS_INFO(ns, "Empty config file!");
+		fclose(config_file);
 		goto parse_error;
 	} else if (file_size > 3 * pow(1024, 2)) { // 3 MiB
 		NS_INFO(ns, "Huge config file! (%f MiB!!) Something's wrong here.", ((float)file_size) / pow(1024, 2));
+		fclose(config_file);
 		goto parse_error;
 	}
 
 	char *json = calloc(file_size + 1, 1);
 	json_allocated = true;
 
-	fread(json, 1, file_size, config_file);
+	(void)fread(json, 1, file_size, config_file);
 	fclose(config_file);
 	json[file_size] = '\0';
 
