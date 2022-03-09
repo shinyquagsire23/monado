175ff58f10079c38b1507342754c4261a56f8f84
Jakob Bornecrantz <jakob@collabora.com>
2022-03-09 22:54:33 +0000

    build: Remove meson files

--- .gitlab-ci/install-cross.sh
+++ .gitlab-ci/install-cross.sh
@@ -57,16 +57,3 @@ done
 
 apt-get autoremove -y --purge
 apt-get clean
-
-# Generate cross build files for Meson
-for arch in $CROSS_ARCHITECTURES; do
-  cross_file="/cross_file-$arch.txt"
-  /usr/share/meson/debcrossgen --arch "$arch" -o "$cross_file"
-  if [ "$arch" = "i386" ]; then
-    # Work around a bug in debcrossgen that should be fixed in the next release
-    sed -i "s|cpu_family = 'i686'|cpu_family = 'x86'|g" "$cross_file"
-  fi
-
-  # Rely on qemu-user being configured in binfmt_misc on the host
-  sed -i -e '/\[properties\]/a\' -e "needs_exe_wrapper = False" "$cross_file"
-done
