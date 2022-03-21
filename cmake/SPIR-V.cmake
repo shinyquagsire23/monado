8d660b6994d9388cafcef36b71defff57667104f
Jakob Bornecrantz <jakob@collabora.com>
2022-03-21 22:42:27 +0000

    cmake: Make it possible to select SPIR-V version to target

--- cmake/SPIR-V.cmake
+++ cmake/SPIR-V.cmake
@@ -8,29 +8,28 @@ if(NOT GLSLANGVALIDATOR_COMMAND)
 endif()
 
 #
-# Generate a SPIR-V header file, with the given var name. Returns the header.
-#
-function(spirv_shader ret GLSL VAR)
-	set(HEADER "${CMAKE_CURRENT_BINARY_DIR}/${GLSL}.h")
-	set(GLSL "${CMAKE_CURRENT_SOURCE_DIR}/${GLSL}")
-
-	add_custom_command(
-		OUTPUT ${HEADER}
-		COMMAND ${GLSLANGVALIDATOR_COMMAND} -V ${GLSL} --vn ${VAR} -o ${HEADER}
-		DEPENDS ${GLSL})
-
-
-	set(${ret} "${HEADER}" PARENT_SCOPE)
-endfunction(spirv_shader)
-
-#
 # Generate SPIR-V header files from the arguments. Returns a list of headers.
 #
 function(spirv_shaders ret)
+	set(options)
+	set(oneValueArgs SPIRV_VERSION)
+	set(multiValueArgs SOURCES)
+	cmake_parse_arguments(_spirvshaders "${options}" "${oneValueArgs}"
+	                      "${multiValueArgs}" ${ARGN})
 
-	foreach(GLSL ${ARGN})
+	if(NOT _spirvshaders_SPIRV_VERSION)
+		set(_spirvshaders_SPIRV_VERSION 1.0)
+	endif()
+
+	foreach(GLSL ${_spirvshaders_SOURCES})
 		string(MAKE_C_IDENTIFIER ${GLSL} IDENTIFIER)
-		spirv_shader(HEADER ${GLSL} ${IDENTIFIER})
+		set(HEADER "${CMAKE_CURRENT_BINARY_DIR}/${GLSL}.h")
+		set(GLSL "${CMAKE_CURRENT_SOURCE_DIR}/${GLSL}")
+
+		add_custom_command(
+			OUTPUT ${HEADER}
+			COMMAND ${GLSLANGVALIDATOR_COMMAND} -V --target-env spirv${_spirvshaders_SPIRV_VERSION} ${GLSL} --vn ${IDENTIFIER} -o ${HEADER}
+			DEPENDS ${GLSL})
 		list(APPEND HEADERS ${HEADER})
 	endforeach()
 
