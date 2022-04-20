aa4f43b0493e9cc0fde8114334bc39d9298f473b
Ryan Pavlik <ryan.pavlik@collabora.com>
2022-04-20 16:04:26 -0500

    cmake: Improve manifest installation, allowing relative paths to be generated.

--- src/xrt/targets/openxr/make_manifest.cmake
+++ src/xrt/targets/openxr/make_manifest.cmake
@@ -1,12 +1,18 @@
-# Copyright 2019-2020, Collabora, Ltd.
+# Copyright 2019-2022, Collabora, Ltd.
 # Copyright 2019, Benjamin Saunders <ben.e.saunders@gmail.com>
 # SPDX-License-Identifier: BSL-1.0
 
 # Get input from main CMake script
 set(MANIFEST_INPUT @MANIFEST_INPUT@)
+# Manifest install dir relative to install prefix
 set(MANIFEST_RELATIVE_DIR @MANIFEST_RELATIVE_DIR@)
+# Runtime install dir relative to install prefix
 set(RUNTIME_RELATIVE_DIR @RUNTIME_RELATIVE_DIR@)
+# Runtime so/dll filename
 set(RUNTIME_FILENAME @RUNTIME_FILENAME@)
+# The relative path from the manifest dir to the runtime. Optional.
+set(RUNTIME_DIR_RELATIVE_TO_MANIFEST @RUNTIME_DIR_RELATIVE_TO_MANIFEST@)
+# Config option
 set(XRT_OPENXR_INSTALL_ABSOLUTE_RUNTIME_PATH @XRT_OPENXR_INSTALL_ABSOLUTE_RUNTIME_PATH@)
 
 # Remove trailing slash
@@ -18,8 +24,11 @@ if(XRT_OPENXR_INSTALL_ABSOLUTE_RUNTIME_PATH)
 	if(NOT IS_ABSOLUTE ${RUNTIME_RELATIVE_DIR})
 		set(RUNTIME_PATH ${CMAKE_INSTALL_PREFIX}/${RUNTIME_PATH})
 	endif()
+elseif(RUNTIME_DIR_RELATIVE_TO_MANIFEST)
+	# Relative path to runtime.
+	set(RUNTIME_PATH ${RUNTIME_DIR_RELATIVE_TO_MANIFEST}/${RUNTIME_FILENAME})
 else()
-	# Relative path to runtime: requires it exist on the system shared library search path.
+	# Unqualified runtime filename: requires it exist on the system shared library search path.
 	set(RUNTIME_PATH ${RUNTIME_FILENAME})
 endif()
 
