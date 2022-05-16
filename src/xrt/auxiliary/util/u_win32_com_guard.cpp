213f4b600336ca56d21b42604de1c237e421ab34
Ryan Pavlik <ryan.pavlik@collabora.com>
2022-05-16 10:32:00 -0500

    a/util: Add a COM guard object.
    Need COM initialized to do some things (like d3d11) on Windows,
    don't know if the app already initialized it, don't have a big preference
    for "how" we initialize it.

---
+++ src/xrt/auxiliary/util/u_win32_com_guard.cpp
@@ -0,0 +1,31 @@
+// Copyright 2022, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief An object that serves to keep the reference count of COM initialization above 0.
+ *
+ * @author Ryan Pavlik <ryan.pavlik@collabora.com>
+ * @ingroup aux_util
+ *
+ */
+#include "u_win32_com_guard.hpp"
+#include "xrt/xrt_config_have.h"
+
+
+#if defined(XRT_OS_WINDOWS) && defined(XRT_HAVE_WIL)
+#include <wil/result.h>
+
+namespace xrt::auxiliary::util {
+
+
+ComGuard::ComGuard()
+{
+	THROW_IF_FAILED(CoIncrementMTAUsage(&m_cookie));
+}
+ComGuard::~ComGuard()
+{
+	LOG_IF_FAILED(CoDecrementMTAUsage(m_cookie));
+}
+
+} // namespace xrt::auxiliary::util
+#endif // defined(XRT_OS_WINDOWS) && defined(XRT_HAVE_WIL)
---
+++ src/xrt/auxiliary/util/u_win32_com_guard.hpp
@@ -0,0 +1,47 @@
+// Copyright 2022, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief An object that serves to keep the reference count of COM initialization above 0.
+ *
+ * @author Ryan Pavlik <ryan.pavlik@collabora.com>
+ * @ingroup aux_util
+ *
+ */
+#pragma once
+
+#include "xrt/xrt_config_os.h"
+
+#if defined(XRT_OS_WINDOWS)
+
+#include "xrt/xrt_windows.h"
+#include "combaseapi.h"
+
+namespace xrt::auxiliary::util {
+
+/**
+ * This object makes sure that Windows doesn't close out COM while we're holding on to COM objects.
+ *
+ * We don't know if the calling thread has initialized COM or how, so this just increments the ref count without
+ * really expressing an opinion.
+ */
+class ComGuard
+{
+public:
+	ComGuard();
+	~ComGuard();
+
+	ComGuard(ComGuard const &) = delete;
+	ComGuard(ComGuard &&) = delete;
+	ComGuard &
+	operator=(ComGuard const &) = delete;
+	ComGuard &
+	operator=(ComGuard &&) = delete;
+
+private:
+	CO_MTA_USAGE_COOKIE m_cookie;
+};
+
+} // namespace xrt::auxiliary::util
+
+#endif // defined(XRT_OS_WINDOWS) && defined(XRT_HAVE_WIL)
