f4cc2f3bf42b6c09f33af7bd4cac3ac1812d3a79
Ryan Pavlik <ryan.pavlik@collabora.com>
2022-07-01 11:39:11 -0500

    cmake: Fix some details of Clang on Windows.

--- CompilerFlags.cmake
+++ CompilerFlags.cmake
@@ -4,10 +4,14 @@
 if(NOT MSVC)
 	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pedantic -Wall -Wextra -Wno-unused-parameter")
 	set(CMAKE_C_FLAGS
-		"${CMAKE_C_FLAGS} -Werror-implicit-function-declaration -Werror=incompatible-pointer-types"
+	    "${CMAKE_C_FLAGS} -Werror-implicit-function-declaration -Werror=incompatible-pointer-types"
 		)
 	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror=int-conversion")
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pedantic -Wall -Wextra -Wno-unused-parameter")
+endif()
+
+if(NOT WIN32)
+	# Even clang's gnu-style driver on windows doesn't accept this argument.
 
 	set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-undefined")
 	set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -Wl,--no-undefined")
