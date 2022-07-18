ffb0346ba4ebed71902e8dabc7b935460d9a707d
Christoph Haag <christoph.haag@collabora.com>
2022-07-18 20:29:35 +0200

    d/survive: Fix compilation on clang
    Fixes error
    
    /home/haagch-collabora/monado/src/xrt/drivers/survive/survive_driver.c:384:53: error: passing 'const union xrt_output_value *' to parameter of type 'union xrt_output_value *' discards qualifiers [-Werror,-Wincompatible-pointer-types-discards-qualifiers]
            int ret = survive_controller_haptic_pulse(survive, value);
                                                               ^~~~~
    /home/haagch-collabora/monado/src/xrt/drivers/survive/survive_driver.c:340:89: note: passing argument to parameter 'value' here
    survive_controller_haptic_pulse(struct survive_device *survive, union xrt_output_value *value)

--- src/xrt/drivers/survive/survive_driver.c
+++ src/xrt/drivers/survive/survive_driver.c
@@ -337,7 +337,7 @@ survive_device_get_tracked_pose(struct xrt_device *xdev,
 }
 
 static int
-survive_controller_haptic_pulse(struct survive_device *survive, union xrt_output_value *value)
+survive_controller_haptic_pulse(struct survive_device *survive, const union xrt_output_value *value)
 {
 	float duration_seconds;
 	if (value->vibration.duration_ns == XRT_MIN_HAPTIC_DURATION) {
