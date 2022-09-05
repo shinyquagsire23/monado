c0a3d53c0a17c09f361d2f0ca40d3d0a048b269d
Jakob Bornecrantz <jakob@collabora.com>
2022-09-05 19:39:10 +0100

    c/main: Only create not begin peek command buffer

--- src/xrt/compositor/main/comp_window_peek.c
+++ src/xrt/compositor/main/comp_window_peek.c
@@ -177,9 +177,9 @@ comp_window_peek_create(struct comp_compositor *c)
 		COMP_ERROR(c, "vkCreateSemaphore: %s", vk_result_string(ret));
 	}
 
-	ret = vk_cmd_buffer_create_and_begin(vk, &w->cmd);
+	ret = vk_cmd_buffer_create(vk, &w->cmd);
 	if (ret != VK_SUCCESS) {
-		COMP_ERROR(c, "vk_cmd_buffer_create_and_begin: %s", vk_result_string(ret));
+		COMP_ERROR(c, "vk_cmd_buffer_create: %s", vk_result_string(ret));
 	}
 
 	os_thread_helper_init(&w->oth);
