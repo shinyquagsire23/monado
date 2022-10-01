e409264ba7ac530e50d2d5db8b7aad64987f7510
Jan Schmidt <jan@centricular.com>
2022-10-02 03:57:19 +1100

    d/wmr: Fix Reverb G2 detection
    196bf24276d7965636b845813b3caff42177f027 broke Reverb G2
    detection. Use the right PID to match the Reverb G2 headset
    again.

--- src/xrt/drivers/wmr/wmr_prober.c
+++ src/xrt/drivers/wmr/wmr_prober.c
@@ -64,7 +64,7 @@ check_and_get_interface(struct xrt_prober_device *device,
 
 		if (device->product_id == REVERB_G1_PID)
 			*out_hmd_type = WMR_HEADSET_REVERB_G1;
-		else if (device->product_id == REVERB_G2_CONTROLLER_PID)
+		else if (device->product_id == REVERB_G2_PID)
 			*out_hmd_type = WMR_HEADSET_REVERB_G2;
 		else if (device->product_id == VR1000_PID)
 			*out_hmd_type = WMR_HEADSET_HP_VR1000;
