b15ac35e766a6f44e80d92932a6e9b06d4ca8390
Jakob Bornecrantz <jakob@collabora.com>
2023-03-03 18:47:49 +0000

    st/oxr: Refactor some things into oxr_defines.h

--- src/xrt/state_trackers/oxr/oxr_objects.h
+++ src/xrt/state_trackers/oxr/oxr_objects.h
@@ -28,6 +28,7 @@
 
 #include "oxr_extension_support.h"
 #include "oxr_subaction.h"
+#include "oxr_defines.h"
 
 #if defined(XRT_HAVE_D3D11) || defined(XRT_HAVE_D3D12)
 #include <dxgi.h>
@@ -86,21 +87,6 @@ extern "C" {
  * @{
  */
 
-// For corruption and layer checking.
-// clang-format off
-#define OXR_XR_DEBUG_INSTANCE  (*(uint64_t *)"oxrinst\0")
-#define OXR_XR_DEBUG_SESSION   (*(uint64_t *)"oxrsess\0")
-#define OXR_XR_DEBUG_SPACE     (*(uint64_t *)"oxrspac\0")
-#define OXR_XR_DEBUG_PATH      (*(uint64_t *)"oxrpath\0")
-#define OXR_XR_DEBUG_ACTION    (*(uint64_t *)"oxracti\0")
-#define OXR_XR_DEBUG_SWAPCHAIN (*(uint64_t *)"oxrswap\0")
-#define OXR_XR_DEBUG_ACTIONSET (*(uint64_t *)"oxraset\0")
-#define OXR_XR_DEBUG_MESSENGER (*(uint64_t *)"oxrmess\0")
-#define OXR_XR_DEBUG_SOURCESET (*(uint64_t *)"oxrsrcs\0")
-#define OXR_XR_DEBUG_SOURCE    (*(uint64_t *)"oxrsrc_\0")
-#define OXR_XR_DEBUG_HTRACKER  (*(uint64_t *)"oxrhtra\0")
-// clang-format on
-
 
 /*
  *
@@ -145,55 +131,6 @@ struct time_state;
  */
 typedef XrResult (*oxr_handle_destroyer)(struct oxr_logger *log, struct oxr_handle_base *hb);
 
-/*!
- * State of a handle base, to reduce likelihood of going "boom" on
- * out-of-order destruction or other unsavory behavior.
- */
-enum oxr_handle_state
-{
-	/*! State during/before oxr_handle_init, or after failure */
-	OXR_HANDLE_STATE_UNINITIALIZED = 0,
-
-	/*! State after successful oxr_handle_init */
-	OXR_HANDLE_STATE_LIVE,
-
-	/*! State after successful oxr_handle_destroy */
-	OXR_HANDLE_STATE_DESTROYED,
-};
-
-/*!
- * Sub action paths.
- */
-enum oxr_subaction_path
-{
-	OXR_SUB_ACTION_PATH_USER,
-	OXR_SUB_ACTION_PATH_HEAD,
-	OXR_SUB_ACTION_PATH_LEFT,
-	OXR_SUB_ACTION_PATH_RIGHT,
-	OXR_SUB_ACTION_PATH_GAMEPAD,
-};
-
-/*!
- * Region of a dpad binding that an input is mapped to
- */
-enum oxr_dpad_region
-{
-	OXR_DPAD_REGION_CENTER = 0u,
-	OXR_DPAD_REGION_UP = (1u << 0u),
-	OXR_DPAD_REGION_DOWN = (1u << 1u),
-	OXR_DPAD_REGION_LEFT = (1u << 2u),
-	OXR_DPAD_REGION_RIGHT = (1u << 3u),
-};
-
-/*!
- * Tracks the state of a image that belongs to a @ref oxr_swapchain.
- */
-enum oxr_image_state
-{
-	OXR_IMAGE_STATE_READY,
-	OXR_IMAGE_STATE_ACQUIRED,
-	OXR_IMAGE_STATE_WAITED,
-};
 
 
 /*
@@ -2013,16 +1950,6 @@ struct oxr_action_attachment
  * @}
  */
 
-enum oxr_space_type
-{
-	OXR_SPACE_TYPE_REFERENCE_VIEW = 1,
-	OXR_SPACE_TYPE_REFERENCE_LOCAL = 2,
-	OXR_SPACE_TYPE_REFERENCE_STAGE = 3,
-	OXR_SPACE_TYPE_REFERENCE_UNBOUNDED_MSFT,
-	OXR_SPACE_TYPE_REFERENCE_COMBINED_EYE_VARJO,
-
-	OXR_SPACE_TYPE_ACTION,
-};
 
 static inline bool
 oxr_space_type_is_reference(enum oxr_space_type space_type)
