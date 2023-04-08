5f1b54bcd5c67a2cd1a718430ee2953f9c476bfe
Jakob Bornecrantz <jakob@collabora.com>
2023-04-08 23:28:44 +0100

    a/vk: Add blit and copy helpers

--- src/xrt/auxiliary/vk/vk_cmd.c
+++ src/xrt/auxiliary/vk/vk_cmd.c
@@ -147,3 +147,339 @@ out:
 
 	return ret;
 }
+
+
+/*
+ *
+ * Command writing functions.
+ *
+ */
+
+void
+vk_cmd_copy_image_locked(struct vk_bundle *vk, VkCommandBuffer cmd_buffer, const struct vk_cmd_copy_image_info *info)
+{
+	VkPipelineStageFlags src_stage_mask = 0;
+	src_stage_mask |= info->src.src_stage_mask;
+	src_stage_mask |= info->dst.src_stage_mask;
+
+	VkImageMemoryBarrier barriers[2] = {
+	    {
+	        .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+	        .srcAccessMask = info->src.src_access_mask,
+	        .dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT,
+	        .oldLayout = info->src.old_layout,
+	        .newLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+	        .image = info->src.fm_image.image,
+	        .subresourceRange =
+	            {
+	                .aspectMask = info->src.fm_image.aspect_mask,
+	                .baseMipLevel = 0,
+	                .levelCount = 1,
+	                .baseArrayLayer = info->src.fm_image.base_array_layer,
+	                .layerCount = 1,
+	            },
+	    },
+	    {
+	        .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+	        .srcAccessMask = info->dst.src_access_mask,
+	        .dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT,
+	        .oldLayout = info->dst.old_layout,
+	        .newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+	        .image = info->dst.fm_image.image,
+	        .subresourceRange =
+	            {
+	                .aspectMask = info->dst.fm_image.aspect_mask,
+	                .baseMipLevel = 0,
+	                .levelCount = 1,
+	                .baseArrayLayer = info->dst.fm_image.base_array_layer,
+	                .layerCount = 1,
+	            },
+	    },
+	};
+
+	vk->vkCmdPipelineBarrier(           //
+	    cmd_buffer,                     // commandBuffer
+	    src_stage_mask,                 // srcStageMask
+	    VK_PIPELINE_STAGE_TRANSFER_BIT, // dstStageMask
+	    0,                              // dependencyFlags
+	    0,                              // memoryBarrierCount
+	    NULL,                           // pMemoryBarriers
+	    0,                              // bufferMemoryBarrierCount
+	    NULL,                           // pBufferMemoryBarriers
+	    ARRAY_SIZE(barriers),           // imageMemoryBarrierCount
+	    barriers);                      // pImageMemoryBarriers
+
+	// Specify the source region to copy from.
+	VkImageSubresourceLayers src_subresource = {
+	    .aspectMask = info->src.fm_image.aspect_mask,
+	    .mipLevel = 0,
+	    .baseArrayLayer = info->src.fm_image.base_array_layer,
+	    .layerCount = 1,
+	};
+
+	// Specify the destination region to copy to.
+	VkImageSubresourceLayers dst_subresource = {
+	    .aspectMask = info->dst.fm_image.aspect_mask,
+	    .mipLevel = 0,
+	    .baseArrayLayer = info->dst.fm_image.base_array_layer,
+	    .layerCount = 1,
+	};
+
+	// Region to copy.
+	VkImageCopy copy_region = {
+	    .extent =
+	        {
+	            .width = info->size.w,  // Width of the region to copy.
+	            .height = info->size.h, // Height of the region to copy.
+	            .depth = 1,
+	        },
+	    .srcSubresource = src_subresource,
+	    .srcOffset = {0, 0, 0},
+	    .dstSubresource = dst_subresource,
+	    .dstOffset = {0, 0, 0},
+	};
+
+	vk->vkCmdCopyImage(                       //
+	    cmd_buffer,                           //
+	    info->src.fm_image.image,             //
+	    VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, //
+	    info->dst.fm_image.image,             //
+	    VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, //
+	    1,                                    //
+	    &copy_region);                        //
+}
+
+void
+vk_cmd_blit_image_locked(struct vk_bundle *vk, VkCommandBuffer cmd_buffer, const struct vk_cmd_blit_image_info *info)
+{
+	VkPipelineStageFlags src_stage_mask = 0;
+	src_stage_mask |= info->src.src_stage_mask;
+	src_stage_mask |= info->dst.src_stage_mask;
+
+	VkImageMemoryBarrier barriers[2] = {
+	    {
+	        .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+	        .srcAccessMask = info->src.src_access_mask,
+	        .dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT,
+	        .oldLayout = info->src.old_layout,
+	        .newLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+	        .image = info->src.fm_image.image,
+	        .subresourceRange =
+	            {
+	                .aspectMask = info->src.fm_image.aspect_mask,
+	                .baseMipLevel = 0,
+	                .levelCount = 1,
+	                .baseArrayLayer = info->src.fm_image.base_array_layer,
+	                .layerCount = 1,
+	            },
+	    },
+	    {
+	        .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+	        .srcAccessMask = info->dst.src_access_mask,
+	        .dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT,
+	        .oldLayout = info->dst.old_layout,
+	        .newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+	        .image = info->dst.fm_image.image,
+	        .subresourceRange =
+	            {
+	                .aspectMask = info->dst.fm_image.aspect_mask,
+	                .baseMipLevel = 0,
+	                .levelCount = 1,
+	                .baseArrayLayer = info->dst.fm_image.base_array_layer,
+	                .layerCount = 1,
+	            },
+	    },
+	};
+
+	vk->vkCmdPipelineBarrier(           //
+	    cmd_buffer,                     // commandBuffer
+	    src_stage_mask,                 // srcStageMask
+	    VK_PIPELINE_STAGE_TRANSFER_BIT, // dstStageMask
+	    0,                              // dependencyFlags
+	    0,                              // memoryBarrierCount
+	    NULL,                           // pMemoryBarriers
+	    0,                              // bufferMemoryBarrierCount
+	    NULL,                           // pBufferMemoryBarriers
+	    ARRAY_SIZE(barriers),           // imageMemoryBarrierCount
+	    barriers);                      // pImageMemoryBarriers
+
+	// Specify the source region to blit from.
+	VkImageSubresourceLayers src_subresource = {
+	    .aspectMask = info->src.fm_image.aspect_mask,
+	    .mipLevel = 0,
+	    .baseArrayLayer = info->src.fm_image.base_array_layer,
+	    .layerCount = 1,
+	};
+
+	// Specify the destination region to blit to.
+	VkImageSubresourceLayers dst_subresource = {
+	    .aspectMask = info->dst.fm_image.aspect_mask,
+	    .mipLevel = 0,
+	    .baseArrayLayer = info->dst.fm_image.base_array_layer,
+	    .layerCount = 1,
+	};
+
+	int src_w1 = info->src.rect.offset.w;
+	int src_h1 = info->src.rect.offset.h;
+	int src_w2 = src_w1 + info->src.rect.extent.w;
+	int src_h2 = src_h1 + info->src.rect.extent.h;
+
+	int dst_w1 = info->dst.rect.offset.w;
+	int dst_h1 = info->dst.rect.offset.h;
+	int dst_w2 = dst_w1 + info->dst.rect.extent.w;
+	int dst_h2 = dst_h1 + info->dst.rect.extent.h;
+
+	VkImageBlit blit_region = {
+	    .srcSubresource = src_subresource,
+	    .srcOffsets[0] = {src_w1, src_h1, 0},
+	    .srcOffsets[1] = {src_w2, src_h2, 1},
+	    .dstSubresource = dst_subresource,
+	    .dstOffsets[0] = {dst_w1, dst_h1, 0},
+	    .dstOffsets[1] = {dst_w2, dst_h2, 1},
+	};
+
+	vk->vkCmdBlitImage(                       //
+	    cmd_buffer,                           //
+	    info->src.fm_image.image,             //
+	    VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, //
+	    info->dst.fm_image.image,             //
+	    VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, //
+	    1,                                    //
+	    &blit_region,                         //
+	    VK_FILTER_LINEAR);                    //
+}
+
+void
+vk_cmd_blit_images_side_by_side_locked(struct vk_bundle *vk,
+                                       VkCommandBuffer cmd_buffer,
+                                       const struct vk_cmd_blit_images_side_by_side_info *info)
+{
+	VkPipelineStageFlags src_stage_mask = 0;
+	src_stage_mask |= info->src[0].src_stage_mask;
+	src_stage_mask |= info->src[1].src_stage_mask;
+	src_stage_mask |= info->dst.src_stage_mask;
+
+	VkImageMemoryBarrier barriers[3] = {
+	    {
+	        .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+	        .srcAccessMask = info->src[0].src_access_mask,
+	        .dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT,
+	        .oldLayout = info->src[0].old_layout,
+	        .newLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+	        .image = info->src[0].fm_image.image,
+	        .subresourceRange =
+	            {
+	                .aspectMask = info->src[0].fm_image.aspect_mask,
+	                .baseMipLevel = 0,
+	                .levelCount = 1,
+	                .baseArrayLayer = info->src[0].fm_image.base_array_layer,
+	                .layerCount = 1,
+	            },
+	    },
+	    {
+	        .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+	        .srcAccessMask = info->src[1].src_access_mask,
+	        .dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT,
+	        .oldLayout = info->src[1].old_layout,
+	        .newLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+	        .image = info->src[1].fm_image.image,
+	        .subresourceRange =
+	            {
+	                .aspectMask = info->src[1].fm_image.aspect_mask,
+	                .baseMipLevel = 0,
+	                .levelCount = 1,
+	                .baseArrayLayer = info->src[1].fm_image.base_array_layer,
+	                .layerCount = 1,
+	            },
+	    },
+	    {
+	        .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+	        .srcAccessMask = info->dst.src_access_mask,
+	        .dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT,
+	        .oldLayout = info->dst.old_layout,
+	        .newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+	        .image = info->dst.fm_image.image,
+	        .subresourceRange =
+	            {
+	                .aspectMask = info->dst.fm_image.aspect_mask,
+	                .baseMipLevel = 0,
+	                .levelCount = 1,
+	                .baseArrayLayer = info->dst.fm_image.base_array_layer,
+	                .layerCount = 1,
+	            },
+	    },
+	};
+
+	vk->vkCmdPipelineBarrier(           //
+	    cmd_buffer,                     // commandBuffer
+	    src_stage_mask,                 // srcStageMask
+	    VK_PIPELINE_STAGE_TRANSFER_BIT, // dstStageMask
+	    0,                              // dependencyFlags
+	    0,                              // memoryBarrierCount
+	    NULL,                           // pMemoryBarriers
+	    0,                              // bufferMemoryBarrierCount
+	    NULL,                           // pBufferMemoryBarriers
+	    ARRAY_SIZE(barriers),           // imageMemoryBarrierCount
+	    barriers);                      // pImageMemoryBarriers
+
+	// Specify the destination region to blit to.
+	VkImageSubresourceLayers dst_subresource = {
+	    .aspectMask = info->dst.fm_image.aspect_mask,
+	    .mipLevel = 0,
+	    .baseArrayLayer = info->dst.fm_image.base_array_layer,
+	    .layerCount = 1,
+	};
+
+	int dst_y0 = 0;
+	int dst_y1 = info->dst.size.h;
+	int dst0_x0 = 0;
+	int dst0_x1 = info->dst.size.w / 2;
+	int dst1_x0 = info->dst.size.w / 2;
+	int dst1_x1 = info->dst.size.w;
+
+	VkOffset3D dst_offset[2][2] = {
+	    {
+	        {dst0_x0, dst_y0, 0},
+	        {dst0_x1, dst_y1, 1},
+	    },
+	    {
+	        {dst1_x0, dst_y0, 0},
+	        {dst1_x1, dst_y1, 1},
+	    },
+	};
+
+	for (int i = 0; i < 2; i++) {
+
+		// Specify the source region to blit from.
+		VkImageSubresourceLayers src_subresource = {
+		    .aspectMask = info->src[i].fm_image.aspect_mask,
+		    .mipLevel = 0,
+		    .baseArrayLayer = info->src[i].fm_image.base_array_layer,
+		    .layerCount = 1,
+		};
+
+		int w1 = info->src[i].rect.offset.w;
+		int h1 = info->src[i].rect.offset.h;
+		int w2 = w1 + info->src[i].rect.extent.w;
+		int h2 = h1 + info->src[i].rect.extent.h;
+
+		VkImageBlit blit_region = {
+		    .srcSubresource = src_subresource,
+		    .srcOffsets[0] = {w1, h1, 0},
+		    .srcOffsets[1] = {w2, h2, 1},
+		    .dstSubresource = dst_subresource,
+		    .dstOffsets[0] = dst_offset[i][0],
+		    .dstOffsets[1] = dst_offset[i][1],
+		};
+
+		vk->vkCmdBlitImage(                       //
+		    cmd_buffer,                           //
+		    info->src[i].fm_image.image,          //
+		    VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, //
+		    info->dst.fm_image.image,             //
+		    VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, //
+		    1,                                    //
+		    &blit_region,                         //
+		    VK_FILTER_LINEAR);                    //
+	}
+}
--- src/xrt/auxiliary/vk/vk_cmd.h
+++ src/xrt/auxiliary/vk/vk_cmd.h
@@ -19,6 +19,107 @@ extern "C" {
 
 /*
  *
+ * Struct(s).
+ *
+ */
+
+/*!
+ * A similar struct to VkImageSubresourceRange and VkImageSubresourceLayers
+ * expect for this it's implied that it's only the first mip-level and only one
+ * array layer used for the operation.
+ *
+ * @ingroup aux_vk
+ */
+struct vk_cmd_first_mip_image
+{
+	uint32_t base_array_layer;
+	VkAccessFlags aspect_mask;
+	VkImage image;
+};
+
+/*!
+ * Argument struct for @ref vk_cmd_copy_image_locked.
+ *
+ * See @ref struct vk_cmd_first_mip_image for array and mip selection rules.
+ *
+ * @ingroup aux_vk
+ */
+struct vk_cmd_copy_image_info
+{
+	struct
+	{
+		VkImageLayout old_layout;
+		VkAccessFlags src_access_mask;
+		VkPipelineStageFlags src_stage_mask;
+		struct vk_cmd_first_mip_image fm_image;
+	} src;
+	struct
+	{
+		VkImageLayout old_layout;
+		VkAccessFlags src_access_mask;
+		VkPipelineStageFlags src_stage_mask;
+		struct vk_cmd_first_mip_image fm_image;
+	} dst;
+	struct xrt_size size;
+};
+
+/*!
+ * Argument struct for @ref vk_cmd_blit_image_locked.
+ *
+ * See @ref struct vk_cmd_first_mip_image for array and mip selection rules.
+ *
+ * @ingroup aux_vk
+ */
+struct vk_cmd_blit_image_info
+{
+	struct
+	{
+		VkImageLayout old_layout;
+		VkAccessFlags src_access_mask;
+		VkPipelineStageFlags src_stage_mask;
+		struct xrt_rect rect;
+		struct vk_cmd_first_mip_image fm_image;
+	} src;
+	struct
+	{
+		VkImageLayout old_layout;
+		VkAccessFlags src_access_mask;
+		VkPipelineStageFlags src_stage_mask;
+		struct xrt_rect rect;
+		struct vk_cmd_first_mip_image fm_image;
+	} dst;
+};
+
+/*!
+ * Argument struct for @ref vk_cmd_blit_images_side_by_side_locked.
+ *
+ * See @ref struct vk_cmd_first_mip_image for array and mip selection rules.
+ *
+ * @ingroup aux_vk
+ */
+struct vk_cmd_blit_images_side_by_side_info
+{
+	struct
+	{
+		VkImageLayout old_layout;
+		VkAccessFlags src_access_mask;
+		VkPipelineStageFlags src_stage_mask;
+		struct xrt_rect rect;
+		struct vk_cmd_first_mip_image fm_image;
+	} src[2];
+	struct
+	{
+		VkImageLayout old_layout;
+		VkAccessFlags src_access_mask;
+		VkPipelineStageFlags src_stage_mask;
+		struct xrt_size size;
+		struct vk_cmd_first_mip_image fm_image;
+	} dst;
+};
+
+
+/*
+ *
  * Command buffer functions.
  *
  */
@@ -85,6 +186,55 @@ vk_cmd_submit_locked(struct vk_bundle *vk, uint32_t count, const VkSubmitInfo *i
  */
 XRT_CHECK_RESULT VkResult
 vk_cmd_end_submit_wait_and_free_cmd_buffer_locked(struct vk_bundle *vk, VkCommandPool pool, VkCommandBuffer cmd_buffer);
+
+
+/*
+ *
+ * Copy and blit helpers.
+ *
+ */
+
+/*!
+ * Performs a copy of a image into a destination image, also does needed barrier
+ * operation needed to get images ready for transfer operations. Images will be
+ * left in the layout and pipeline needed for transfers.
+ *
+ * * Src image(s): VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL
+ * * Dst image(s): VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL
+ *
+ * @ingroup aux_vk
+ */
+void
+vk_cmd_copy_image_locked(struct vk_bundle *vk, VkCommandBuffer cmd_buffer, const struct vk_cmd_copy_image_info *info);
+
+/*!
+ * Performs a blit of a image into a destination image, also does needed barrier
+ * operation needed to get images ready for transfer operations. Images will be
+ * left in the layout and pipeline needed for transfers.
+ *
+ * * Src image(s): VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL
+ * * Dst image(s): VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL
+ *
+ * @ingroup aux_vk
+ */
+void
+vk_cmd_blit_image_locked(struct vk_bundle *vk, VkCommandBuffer cmd_buffer, const struct vk_cmd_blit_image_info *info);
+
+/*!
+ * Performs a blit of two images to side by side on a destination image, also
+ * does needed barrier operation needed to get images ready for transfer
+ * operations. Images will be left in the layout and pipeline needed for
+ * transfers.
+ *
+ * * Src image(s): VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL
+ * * Dst image(s): VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL
+ *
+ * @ingroup aux_vk
+ */
+void
+vk_cmd_blit_images_side_by_side_locked(struct vk_bundle *vk,
+                                       VkCommandBuffer cmd_buffer,
+                                       const struct vk_cmd_blit_images_side_by_side_info *info);
 
 
 #ifdef __cplusplus
