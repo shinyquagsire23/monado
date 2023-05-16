cd79bef0cf16b110850665f95b3a17a327ad1444
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-05-16 16:47:43 -0500

    cmake: Fix build flags on MSVC

--- CompilerFlags.cmake
+++ CompilerFlags.cmake
@@ -1,12 +1,11 @@
 # Copyright 2018-2023, Collabora, Ltd.
 # SPDX-License-Identifier: BSL-1.0
 
-
 # Target used for applying more aggressive optimizations to math-heavy code
 add_library(xrt-optimized-math INTERFACE)
 
 if(MSVC)
-	target_compile_options(xrt-optimized-math INTERFACE $<IF:$<CONFIG:Debug>,/O2,/O3>)
+	target_compile_options(xrt-optimized-math INTERFACE $<IF:$<CONFIG:Debug>,/O2 /Ob2,/O2 /Ob3>)
 else()
 	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pedantic -Wall -Wextra -Wno-unused-parameter")
 	set(CMAKE_C_FLAGS
@@ -31,3 +30,28 @@ if(NOT WIN32)
 	set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-undefined")
 	set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -Wl,--no-undefined")
 endif()
+
+# Must call before adding targets that will use xrt-optimized-math
+macro(xrt_optimized_math_flags)
+	if(MSVC)
+		foreach(
+			FLAGSVAR
+			CMAKE_CXX_FLAGS_DEBUG
+			CMAKE_CXX_FLAGS_RELEASE
+			CMAKE_CXX_FLAGS_RELWITHDEBINFO
+			CMAKE_CXX_FLAGS_MINSIZEREL
+			CMAKE_C_FLAGS_DEBUG
+			CMAKE_C_FLAGS_RELEASE
+			CMAKE_C_FLAGS_RELWITHDEBINFO
+			CMAKE_C_FLAGS_MINSIZEREL
+			)
+
+			string(REPLACE "/Od" "" ${FLAGSVAR} "${${FLAGSVAR}}")
+			string(REPLACE "/O1" "" ${FLAGSVAR} "${${FLAGSVAR}}")
+			string(REPLACE "/O2" "" ${FLAGSVAR} "${${FLAGSVAR}}")
+			string(REPLACE "/Ob1" "" ${FLAGSVAR} "${${FLAGSVAR}}")
+			string(REPLACE "/Ob0" "" ${FLAGSVAR} "${${FLAGSVAR}}")
+			string(REPLACE "/RTC1" "" ${FLAGSVAR} "${${FLAGSVAR}}")
+		endforeach()
+	endif()
+endmacro()
