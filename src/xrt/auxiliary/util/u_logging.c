b33e65698a0e9d8727f4aff14faee80b326a76af
Jakob Bornecrantz <jakob@collabora.com>
2023-06-14 19:12:28 +0100

    u/logging: Refactor logging printing functions

--- src/xrt/auxiliary/util/u_logging.c
+++ src/xrt/auxiliary/util/u_logging.c
@@ -12,10 +12,24 @@
 #include "xrt/xrt_config_build.h"
 
 #include "util/u_debug.h"
+#include "util/u_truncate_printf.h"
 
 #include <assert.h>
 #include <stdio.h>
 #include <stdarg.h>
+
+
+/*
+ *
+ * Defines.
+ *
+ */
+
+/*
+ * Avoid 4K stack variables.
+ * https://learn.microsoft.com/en-us/windows/win32/devnotes/-win32-chkstk
+ */
+#define LOG_BUFFER_SIZE (3 * 1024)
 
 
 /*
@@ -159,7 +173,7 @@ u_log_xdev_hex(const char *file,
 
 /*
  *
- * General logging functions.
+ * Platform specific functions.
  *
  */
 
@@ -182,110 +196,16 @@ u_log_convert_priority(enum u_logging_level level)
 	return ANDROID_LOG_INFO;
 }
 
-void
-u_log(const char *file, int line, const char *func, enum u_logging_level level, const char *format, ...)
-{
-	// print_prefix(func, level);
-	android_LogPriority prio = u_log_convert_priority(level);
-	va_list args;
-	va_start(args, format);
-	DISPATCH_SINK(file, line, func, level, format, args);
-	__android_log_vprint(prio, func, format, args);
-	va_end(args);
-}
-
-void
-u_log_xdev(const char *file,
-           int line,
-           const char *func,
-           enum u_logging_level level,
-           struct xrt_device *xdev,
-           const char *format,
-           ...)
-{
-	android_LogPriority prio = u_log_convert_priority(level);
-	va_list args;
-	va_start(args, format);
-	DISPATCH_SINK(file, line, func, level, format, args);
-	__android_log_vprint(prio, func, format, args);
-	va_end(args);
-}
-
-
 #elif defined(XRT_OS_WINDOWS)
 
 #include <debugapi.h>
 
-static int
-print_prefix(int remainingBuf, char *buf, const char *func, enum u_logging_level level)
-{
-	int printed = 0;
-	switch (level) {
-	case U_LOGGING_TRACE: printed = sprintf_s(buf, remainingBuf, "TRACE "); break;
-	case U_LOGGING_DEBUG: printed = sprintf_s(buf, remainingBuf, "DEBUG "); break;
-	case U_LOGGING_INFO: printed = sprintf_s(buf, remainingBuf, " INFO "); break;
-	case U_LOGGING_WARN: printed = sprintf_s(buf, remainingBuf, " WARN "); break;
-	case U_LOGGING_ERROR: printed = sprintf_s(buf, remainingBuf, "ERROR "); break;
-	case U_LOGGING_RAW: break;
-	default: break;
-	}
-
-	if (level != U_LOGGING_RAW && func != NULL) {
-		printed += sprintf_s(buf + printed, remainingBuf - printed, "[%s] ", func);
-	}
-	return printed;
-}
-
-void
-u_log(const char *file, int line, const char *func, enum u_logging_level level, const char *format, ...)
-{
-
-	char buf[16384] = {0};
-
-	int remainingBuffer = sizeof(buf) - 2; // 2 for \n\0
-	int printed = print_prefix(remainingBuffer, buf, func, level);
-
-	va_list args;
-	va_start(args, format);
-	DISPATCH_SINK(file, line, func, level, format, args);
-	printed += vsprintf_s(buf + printed, remainingBuffer - printed, format, args);
-	va_end(args);
-	buf[printed++] = '\n';
-	buf[printed++] = '\0';
-	OutputDebugStringA(buf);
-	fprintf(stderr, "%s", buf);
-}
-
-void
-u_log_xdev(const char *file,
-           int line,
-           const char *func,
-           enum u_logging_level level,
-           struct xrt_device *xdev,
-           const char *format,
-           ...)
-{
-
-	char buf[16384] = {0};
-
-	int remainingBuffer = sizeof(buf) - 2; // 2 for \n\0
-	int printed = print_prefix(remainingBuffer, buf, func, level);
-
-	va_list args;
-	va_start(args, format);
-	DISPATCH_SINK(file, line, func, level, format, args);
-	printed += vsprintf_s(buf + printed, remainingBuffer - printed, format, args);
-	va_end(args);
-	buf[printed++] = '\n';
-	buf[printed++] = '\0';
-	OutputDebugStringA(buf);
-	fprintf(stderr, "%s", buf);
-}
-
-
 #else
 
 #include <unistd.h>
+
+#endif
+
 
 /*
  *
@@ -293,6 +213,16 @@ u_log_xdev(const char *file,
  *
  */
 
+#define CHECK_RET_AND_UPDATE_STATE()                                                                                   \
+	do {                                                                                                           \
+		if (ret < 0) {                                                                                         \
+			return ret;                                                                                    \
+		}                                                                                                      \
+                                                                                                                       \
+		printed += ret; /* Is not negative, checked above. */                                                  \
+		remaining -= ret;                                                                                      \
+		buf += ret; /* Continue in the buffer from where we left off. */                                       \
+	} while (false);
 
 #ifdef XRT_FEATURE_COLOR_LOG
 #define COLOR_TRACE "\033[2m"
@@ -302,51 +232,116 @@ u_log_xdev(const char *file,
 #define COLOR_ERROR "\033[31m"
 #define COLOR_RESET "\033[0m"
 
-static void
-print_prefix_color(const char *func, enum u_logging_level level)
+static int
+print_prefix_color(const char *func, enum u_logging_level level, char *buf, int remaining)
 {
 	switch (level) {
-	case U_LOGGING_TRACE: fprintf(stderr, COLOR_TRACE "TRACE " COLOR_RESET); break;
-	case U_LOGGING_DEBUG: fprintf(stderr, COLOR_DEBUG "DEBUG " COLOR_RESET); break;
-	case U_LOGGING_INFO: fprintf(stderr, COLOR_INFO " INFO " COLOR_RESET); break;
-	case U_LOGGING_WARN: fprintf(stderr, COLOR_WARN " WARN " COLOR_RESET); break;
-	case U_LOGGING_ERROR: fprintf(stderr, COLOR_ERROR "ERROR " COLOR_RESET); break;
-	case U_LOGGING_RAW: break;
-	default: break;
+	case U_LOGGING_TRACE: return u_truncate_snprintf(buf, remaining, COLOR_TRACE "TRACE " COLOR_RESET);
+	case U_LOGGING_DEBUG: return u_truncate_snprintf(buf, remaining, COLOR_DEBUG "DEBUG " COLOR_RESET);
+	case U_LOGGING_INFO: return u_truncate_snprintf(buf, remaining, COLOR_INFO " INFO " COLOR_RESET);
+	case U_LOGGING_WARN: return u_truncate_snprintf(buf, remaining, COLOR_WARN " WARN " COLOR_RESET);
+	case U_LOGGING_ERROR: return u_truncate_snprintf(buf, remaining, COLOR_ERROR "ERROR " COLOR_RESET);
+	case U_LOGGING_RAW: return 0;
+	default: return 0;
 	}
 }
 #endif
 
-static void
-print_prefix_mono(const char *func, enum u_logging_level level)
+static int
+print_prefix_mono(const char *func, enum u_logging_level level, char *buf, int remaining)
 {
 	switch (level) {
-	case U_LOGGING_TRACE: fprintf(stderr, "TRACE "); break;
-	case U_LOGGING_DEBUG: fprintf(stderr, "DEBUG "); break;
-	case U_LOGGING_INFO: fprintf(stderr, " INFO "); break;
-	case U_LOGGING_WARN: fprintf(stderr, " WARN "); break;
-	case U_LOGGING_ERROR: fprintf(stderr, "ERROR "); break;
-	case U_LOGGING_RAW: break;
-	default: break;
+	case U_LOGGING_TRACE: return u_truncate_snprintf(buf, remaining, "TRACE ");
+	case U_LOGGING_DEBUG: return u_truncate_snprintf(buf, remaining, "DEBUG ");
+	case U_LOGGING_INFO: return u_truncate_snprintf(buf, remaining, " INFO ");
+	case U_LOGGING_WARN: return u_truncate_snprintf(buf, remaining, " WARN ");
+	case U_LOGGING_ERROR: return u_truncate_snprintf(buf, remaining, "ERROR ");
+	case U_LOGGING_RAW: return 0;
+	default: return 0;
 	}
 }
 
-static void
-print_prefix(const char *func, enum u_logging_level level)
+static int
+print_prefix(const char *func, enum u_logging_level level, char *buf, int remaining)
 {
+	int printed = 0;
+	int ret = 0;
+
 #ifdef XRT_FEATURE_COLOR_LOG
 	if (isatty(STDERR_FILENO)) {
-		print_prefix_color(func, level);
+		ret = print_prefix_color(func, level, buf, remaining);
 	} else {
-		print_prefix_mono(func, level);
+		ret = print_prefix_mono(func, level, buf, remaining);
 	}
 #else
-	print_prefix_mono(func, level);
+	ret = print_prefix_mono(func, level, buf, remaining);
 #endif
 
+	// Does what it says.
+	CHECK_RET_AND_UPDATE_STATE();
+
+	// Print the function name.
 	if (level != U_LOGGING_RAW && func != NULL) {
-		fprintf(stderr, "[%s] ", func);
+		ret = u_truncate_snprintf(buf, remaining, "[%s] ", func);
 	}
+
+	// Does what it says.
+	CHECK_RET_AND_UPDATE_STATE();
+
+	// Total printed characters.
+	return printed;
+}
+
+static int
+do_print(const char *file, int line, const char *func, enum u_logging_level level, const char *format, va_list args)
+{
+	char storage[LOG_BUFFER_SIZE];
+
+	int remaining = sizeof(storage) - 2; // 2 for \n\0
+	int printed = 0;
+	char *buf = storage; // We update the pointer.
+	int ret = 0;
+
+	// The prefix of the log.
+	ret = print_prefix(func, level, buf, remaining);
+
+	// Does what it says.
+	CHECK_RET_AND_UPDATE_STATE();
+
+	ret = u_truncate_vsnprintf(buf, remaining, format, args);
+
+	// Does what it says.
+	CHECK_RET_AND_UPDATE_STATE();
+
+	/*
+	 * The variable storage now holds the entire null-terminated message,
+	 * but without a new-line character, proceed to output it.
+	 */
+	assert(storage[printed] == '\0');
+
+
+#ifdef XRT_OS_ANDROID
+
+	android_LogPriority prio = u_log_convert_priority(level);
+	__android_log_write(prio, func, storage);
+
+#elif defined XRT_OS_WINDOWS || defined XRT_OS_LINUX
+
+#if defined XRT_OS_WINDOWS
+	OutputDebugStringA(storage); // No newline.
+#endif
+
+	// We want a newline, so add it, then null-terminate again.
+	storage[printed++] = '\n';
+	storage[printed] = '\0'; // Don't count zero termination as printed.
+
+	fwrite(storage, printed, 1, stderr);
+
+#else
+#error "Port needed for logging function"
+#endif
+
+	return printed;
 }
 
 
@@ -359,15 +354,11 @@ print_prefix(const char *func, enum u_logging_level level)
 void
 u_log(const char *file, int line, const char *func, enum u_logging_level level, const char *format, ...)
 {
-	print_prefix(func, level);
-
 	va_list args;
 	va_start(args, format);
 	DISPATCH_SINK(file, line, func, level, format, args);
-	vfprintf(stderr, format, args);
+	do_print(file, line, func, level, format, args);
 	va_end(args);
-
-	fprintf(stderr, "\n");
 }
 
 void
@@ -379,14 +370,9 @@ u_log_xdev(const char *file,
            const char *format,
            ...)
 {
-	print_prefix(func, level);
-
 	va_list args;
 	va_start(args, format);
 	DISPATCH_SINK(file, line, func, level, format, args);
-	vfprintf(stderr, format, args);
+	do_print(file, line, func, level, format, args);
 	va_end(args);
-
-	fprintf(stderr, "\n");
 }
-#endif
